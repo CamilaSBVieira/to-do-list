0c16b46c049969eae565cfc696b6fc9014eaa8d7
CamilaSBVieira <camilag2.c@gmail.com>
2023-02-21 17:43:10 -0300

    Change styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
-import logo from '../assets/images/logo.svg'
+import logo from '../assets/images/logo.png'
 import styles from './Header.module.css'
 
 export function Header() {
     return (
         <header className={styles.header}>
-            <img
+            {/* <img
                 className={styles.headerLogo}
                 src={logo}
                 alt='Logo'
-            />
+            /> */}
             <h1 className={styles.headerTitle}>to<span>do</span></h1>
         </header>
     )
